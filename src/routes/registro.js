d77f62d1199253d75e04d0dcee2a9a0b4ddf074b
Laura <lgh15@alu.ua.es>
2020-11-08 00:44:15 +0100

    registro funciona bien

--- src/routes/registro.js
+++ src/routes/registro.js
@@ -12,23 +12,38 @@ router.get('/registro', (req, res)=> {
 
 router.post('/new-user', (req, res) => {
 
-    if(req.body.name != null && req.body.pass != null && req.body.email != null) {
+    if(req.body.name != null && req.body.pass != null && req.body.email != null && req.body.repitepass != null) {
+        db.ref('/usuario').orderByChild('email').equalTo(req.body.email).once('value', function (snapshot) {
+            var encontrado = false;
 
-        if(req.body.email != snapshot.val() && req.body.pass == req.body.repitepass)
-            {
-                var user = {
-                    name: req.body.name,
-                    pass: req.body.pass,
-                    email: req.body.email
-                };
-                console.log(user);
-                db.ref('usuario').push(user);
-                res.redirect('/');
+            snapshot.forEach(function (childSnapshot) {
+                var value = childSnapshot.val();
+                console.log("email : " + value.email);
+        
+                if(req.body.email == value.email){
+                    encontrado = true;
+                }
+            });
+
+            if(!encontrado){
+                if(req.body.pass == req.body.repitepass){
+                    var user = {
+                        name: req.body.name,
+                        pass: req.body.pass,
+                        email: req.body.email
+                    };
+                    db.ref('usuario').push(user);
+                    res.redirect('/');
+                }
+                else{
+                    res.send('Las contraseñas no coinciden');
+                }
             }
             else
             {
-                res.send('Las contraseñas no coinciden');
+                res.send('El email ya existe');
             }       
+        });
     }
     else{
         //res.redirect('/');
